2fe8c3f4cad46d4ceade1cdc7f5c60e864d66bed
Sanskriti Srivastava <137165548+sanskritisrv@users.noreply.github.com>
2025-07-08 16:04:58 +0530

    Time: 79 ms (70.11%) | Memory: 88.7 MB (73.97%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         /*my approach is to store the elemts in the hash map and then check if the [i+1] wala no is present in the map then do count++ but ye thoda gaalt beacuse the array is unsorted */
-        int n=nums.size();
+        //APPROACH 1- TC=0(NLOGN)+0(n); sc=0(n)
+       /* int n=nums.size();
         sort(nums.begin(),nums.end());
         int gloL,currL=1;
         if(n==0) return 0;
@@ -12,10 +13,35 @@ public:
             else {gloL=max(gloL,currL); currL=1;}
         }
         gloL=max(gloL,currL);
-        return gloL;
+        return gloL;*/
 
         
 
-        
+    
+        unordered_set<int> s(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : s) {
+            // Only start counting if it's the start of a sequence
+            if (s.find(num - 1) == s.end()) {
+                int currentNum = num;
+                int streak = 1;
+
+                while (s.find(currentNum + 1) != s.end()) {
+                    currentNum++;
+                    streak++;
+                }
+
+                longest = max(longest, streak);
+            }
+        }
+
+        return longest;
     }
 };
+
+
+        
+
+      
+    
